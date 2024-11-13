dd64d75ec55dc82f9dbfeffa1715f83abbc87320
TuoAdama <tuoadama17@gmail.com>
2024-11-13 14:53:45 +0100

    feat: search command

--- src/Command/SearchRdvCommand.php
+++ src/Command/SearchRdvCommand.php
@@ -2,47 +2,32 @@
 
 namespace App\Command;
 
+use App\Service\ScrapingService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
+use Twilio\Exceptions\ConfigurationException;
 
 #[AsCommand(
-    name: 'SearchRdvCommand',
+    name: 'app:search:rdv',
     description: 'Add a short description for your command',
 )]
 class SearchRdvCommand extends Command
 {
-    public function __construct()
+    public function __construct(
+        private readonly ScrapingService $scrapingService,
+    )
     {
         parent::__construct();
     }
 
-    protected function configure(): void
-    {
-        $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
-    }
-
+    /**
+     * @throws ConfigurationException
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
-
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $this->scrapingService->searchRdv();
 
         return Command::SUCCESS;
     }
